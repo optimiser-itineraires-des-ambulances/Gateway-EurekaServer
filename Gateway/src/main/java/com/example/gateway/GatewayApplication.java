84ccae3bd35f61f0ee7b49a87a1f2f4c39174f25
dohatir <dohagit0@gmail.com>
2024-12-24 12:21:22 +0100

    config

--- Gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
